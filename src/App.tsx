b18947aee8f20c6986a88cf369315bf885a83673
tcharlton321 <timothycharlton321u@gmail.com>
2021-05-18 14:50:29 -0500

    now only updates state once

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Pokemon, PokemonSearchResults } from './react-app-env';
 import './App.css';
 
 function App() {
+  const limit:number = 151;
   const defaultPokemon:Pokemon[] = [];
 
   
@@ -11,28 +12,18 @@ function App() {
   const [loading, setLoading]: [boolean, (loading: boolean) => void] = useState<boolean>(true);
   const [error, setError]: [string, (error: string) => void] = useState("");
 
-  const addPokemon = (poke:Pokemon) => {
-    pokemons.push(poke);
-    pokemons.sort((a, b) => a.id - b.id)
-    setPokemonList([...pokemons]);
-  }
-
   useEffect(() => {
     setLoading(true);
-    axios.get<PokemonSearchResults>('https://pokeapi.co/api/v2/pokemon?limit=151')
+    axios.get<PokemonSearchResults>('https://pokeapi.co/api/v2/pokemon?limit='+limit)
       .then(response_01 => {
         response_01.data.results?.map((re) => (
           axios.get<Pokemon>(re.url)
           .then(response_02 => {
-            addPokemon(response_02.data)
-          })
-          .catch(ex => {
-            const error =
-            ex.response.status === 404
-              ? "Resource Not found"
-              : "An unexpected error has occurred";
-            setError(error);
-            setLoading(false);
+            pokemons.push(response_02.data);
+            if(response_02.data.id == limit){
+              pokemons.sort((a, b) => a.id - b.id)
+              setPokemonList([...pokemons]);
+            }
           })
         ))
       })
